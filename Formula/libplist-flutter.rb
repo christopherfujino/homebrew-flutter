f245ed09216c036230a596ca4fcf67e5f2e46b95
Christopher Fujino <christopherfujino@gmail.com>
2019-05-30 10:11:45 -0700

    default to git

--- Formula/libplist-flutter.rb
+++ Formula/libplist-flutter.rb
@@ -1,25 +1,23 @@
 class LibplistFlutter < Formula
   desc "Library for Apple Binary- and XML-Property Lists"
   homepage "https://www.libimobiledevice.org/"
-  url "https://www.libimobiledevice.org/downloads/libplist-2.0.0.tar.bz2"
-  sha256 "3a7e9694c2d9a85174ba1fa92417cfabaea7f6d19631e544948dc7e17e82f602"
-  revision 1
+  #url "https://www.libimobiledevice.org/downloads/libplist-2.0.0.tar.bz2"
+  #sha256 "3a7e9694c2d9a85174ba1fa92417cfabaea7f6d19631e544948dc7e17e82f602"
+  #revision 1
+  url "https://github.com/christopherfujino/libplist.git"
 
-  bottle do
-    cellar :any
-    sha256 "dc0f0c66bcc89a91e635364158077b303d259ba5ac485726072d6abaae7219b3" => :mojave
-    sha256 "2139142c8404729b61bf4c97d479abbf4f5bca5a6d34c188a393ff871831afb7" => :high_sierra
-    sha256 "4edadbe0762df848bca097da6f8aa147471b6bca80da86a740cede8241e84030" => :sierra
-  end
+  #head do
+  #  #url "https://git.sukimashita.com/libplist.git"
+  #  url "https://github.com/christopherfujino/libplist.git"
 
-  head do
-    #url "https://git.sukimashita.com/libplist.git"
-    url "https://github.com/christopherfujino/libplist.git"
+  #  depends_on "autoconf" => :build
+  #  depends_on "automake" => :build
+  #  depends_on "libtool" => :build
+  #end
 
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
 
   depends_on "pkg-config" => :build
 
